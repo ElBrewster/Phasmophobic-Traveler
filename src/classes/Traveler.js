ffe6b4e1644667d31a8ffffb50ea1d6565d135c6
El Brewster <brewsterel3@gmail.com>
2023-01-13 09:26:00 -0800

    Add some Traveler test and implementation

--- src/classes/Traveler.js
+++ src/classes/Traveler.js
@@ -1,6 +1,22 @@
+class Traveler {
+    constructor(travelerId, allTravelerData) {
+        this.id = travelerId;
+        this.object1 = this.findTravelerObject(allTravelerData);
+    }
+
+    findTravelerObject(allTravelerData) {
+        let travelerObject = allTravelerData.find(element => {
+            return element.id === this.id;
+        })
+        console.log("travelerObject: ", travelerObject);
+        return travelerObject;
+    }
+}
 //instantiate AllTrips to get 1traveler's trips with AllTrips method(s)
 //or do this in Agent class? idk
 
 //use Alltrips.filterOneClientTrips and set as a property for one traveler, matches userId passed in for AllTrips
 //Alltrips will always hand data to user, whether one traveler or an agent; it handles trip data
 //Agent handles classes
+
+export default Traveler;
--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -1,2 +1,50 @@
 import { expect } from "chai";
 import Traveler from "../src/classes/Traveler";
+
+describe("Traveler", () => {
+    let traveler1;
+    let travelerId = 3;
+    const travelersData = [
+        {
+        id: 1,
+        name: "Ham Leadbeater",
+        travelerType: "relaxer"
+        },
+        {
+        id: 2,
+        name: "Rachael Vaughten",
+        travelerType: "thrill-seeker"
+        },
+        {
+        id: 3,
+        name: "Sibby Dawidowitsch",
+        travelerType: "shopper"
+        },
+        {
+        id: 4,
+        name: "Leila Thebeaud",
+        travelerType: "photographer"
+        }];
+
+        beforeEach(function() {
+            traveler1 = new Traveler(travelerId, travelersData);
+        });
+
+        it("Should be a function and instantiate our good friend Traveler", () => {
+            expect(Traveler).to.be.a("function");
+            expect(traveler1).to.be.an.instanceOf(Traveler);
+        });
+    
+        it("Should have a property to store a traveler id", () => {
+            expect(traveler1.id).to.be.equal(3);
+        });
+
+        it("Should have a method to find and instantiate itself using a traveler id passed in as an argument", () => {
+            let method0 = traveler1.findTravelerObject(travelersData);
+            expect(method0).to.be.equal(traveler1.object);
+        });
+    
+        it("Should have a property to store the results of that method", () => {
+            expect(traveler1.object).to.deep.equal(travelersData[2]);
+        });
+})
