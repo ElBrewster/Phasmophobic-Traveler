734075f2a32214907339630e237ef154c3a9965d
El Brewster <brewsterel3@gmail.com>
2023-01-13 22:24:39 -0800

    Add some scripts file fiddling, css tooling, and glide reequirements

--- src/scripts.js
+++ src/scripts.js
@@ -1,37 +1,57 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS (-->SCSS) file
 import './css/styles.scss';
 import "./data/ghost-facts";
+import Glide from '@glidejs/glide'
+// import './images/turing-logo.png'
 
 //-----glide?
-import Glide from '@glidejs/glide'
-
-new Glide('.glide').mount()
+let glide = new Glide('.glide');
+glide.mount()
 //-----glide^?
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
 
 // const windowLoad = document.addEventListener("load", newsTicker);
 
 // const newsTicker = () => {
 //     ghostFacts.forEach(fact => console.log(fact));
 // }
-console.log('This is the JavaScript entry file - your code begins here.');
-// -->w3 schools script for accordion buttions:
-// var acc = document.getElementsByClassName("accordion");
-// var i;
 
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     var panel = this.nextElementSibling;
-//     if (panel.style.maxHeight) {
-//       panel.style.maxHeight = null;
-//     } else {
-//       panel.style.maxHeight = panel.scrollHeight + "px";
-//     } 
-//   });
-// }
+console.log('This is the JavaScript entry file - your code begins here.');
+//-----
+window.addEventListener("load", startUp);
+   
+const startUp = () => {
+    firstBtnLoop();
+    secondBtnLoop();
+};
+
+//----accordion-----
+let acc = document.getElementsByClassName("accordion");
+let i;
+let panel = this.nextElementSibling;
+
+const firstBtnLoop = () => {
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+      });
+    }
+}
+
+const secondBtnLoop = () => {
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+      });
+    }
+}
+
 
